8111d5fbd0b9be12189fb91fe1c2c5f89b3031e2
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-18 15:33:13 -0400

    return items purchased fxn

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -128,8 +128,25 @@ contract NFTMarketplace is ERC721URIStorage {
         payable(owner).transfer(listingPrice);
         payable(seller).transfer(msg.value);
     }
+
     
     // return: unsold items
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _tokenIds.current();
+        uint unsoldItemCount = _tokens.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i =0; i < itemCount; i++) {
+            if (idToMarketItem[i+1].owner == address(this)) {
+                uint currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex +=1;
+            }
+        }
+        return items;
+    }
 
     // Show the users purchased NFTs
 
