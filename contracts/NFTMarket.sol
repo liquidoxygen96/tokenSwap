a805e40fc09d76183ff644941d38fab215343b16
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-17 20:21:43 -0400

    init marketplace contract

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,31 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    address contractAddress;
+    constructor (address marketplaceAddress) ERC721("RUMZVERSE", "RUMZ"){
+        contractAddress = marketplaceAddress;
+    }
+// mint token and retrieve the minted token ID 
+    function createToken(string memory tokenURI) public returns (uint){
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+}
