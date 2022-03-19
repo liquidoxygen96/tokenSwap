080f92c334bd7403c2a5bee3ba0b764fb3b56c65
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-19 18:09:12 -0400

    import packages, add client

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -6,3 +6,41 @@
  * 3. set asking price and add metadata to describe the NFT
  * After creating and listing an item for sale: re-route user to main page
  */
+
+import { useState } from "react";
+import { ethers } from "ethers";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useRouter } from "next/router";
+import Web3Modal from "web3modal";
+
+//create server for IPFS upload on infura.io
+const client = ipfsHttpClient("https://ipfs.infura.io:5001");
+
+import { marketplaceAddress } from "../config";
+
+import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
+
+export default function createItem() {
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, updateFormInput] = useState({
+    price: "",
+    name: "",
+    description: "",
+  });
+  const router = useRouter();
+
+  async function onChange(e) {
+    // upload image to IPFS
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file, {
+        progress: (prog) => console.log("received: ${prog}"),
+      });
+      const url = "https://ipfs.infura.io/ipfs/${added.path}";
+      setFileUrl(url);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
+  async function uploadToIPFS() {}
+}
