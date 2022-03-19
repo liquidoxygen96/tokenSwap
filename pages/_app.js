98151423dbce2e9a0135a8ee43e340acaa47f906
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-19 16:39:58 -0400

    main_page init

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,28 @@ import "../styles/globals.css";
 import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return ();
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">JudoSwap</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">Home</a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">Mint</a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">My NFTs</a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
