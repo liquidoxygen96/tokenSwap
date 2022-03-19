fa1d22f638a6958b226b99d85278b474c413e0a5
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-19 19:14:44 -0400

    my-nfts init page

---
+++ pages/my-nfts.js
@@ -0,0 +1,6 @@
+/**
+ * Page for viewing the NFTs purchased by the User
+ * NFT is retrieved using the fetchMyNFTs function in the solidity contract
+ *
+ * Main function: retrieve NFT owned by the User and render them on the "my-nfts page"
+ */
