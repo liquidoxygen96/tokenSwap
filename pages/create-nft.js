310666ecb070e73b2ff7c443703ee7338a7e9451
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-19 17:43:34 -0400

    create-nft page

---
+++ pages/create-nft.js
@@ -0,0 +1,8 @@
+/**
+ * Page for creating and listing NFTs
+ * main functions of this page:
+ * 1.upload and save files to IPFS
+ * 2.mint the uploaded file as an NFT and list it for sale
+ * 3. set asking price and add metadata to describe the NFT
+ * After creating and listing an item for sale: re-route user to main page
+ */
