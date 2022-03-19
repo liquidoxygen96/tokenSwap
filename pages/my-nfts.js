564e0a14940a7949cc45ec23c533f24f80928598
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-19 19:48:55 -0400

    syntax edit

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -62,24 +62,29 @@ export default function MyAssets() {
     router.push("/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}");
   }
   if (loadingState === "loaded" && !nfts.length)
-    return (<h1 className="py-10 px-20 text-3x1">No NFTs owned yet</h1>)
-    return (
-        <div className='flex justify-center'>
-            <div className='p-4'>
-                <div className='grid grid-cols-1 sm:grid-cols02 lg:grid-cols-4 gap-4 pt-4'>
-                    {
-                        nfts.map((nft,i)) => (
-                            <div key={i} className='border shadow rounded-x1 overflow-hidden'>
-                                <img src={nft.image} className='rounded'/>
-                                <div className='p-4 bg-black'>
-                                    <p className='text-2x1 font-bold text-white'>Price -{nft.price} ETHER</p>
-                                    <button className='mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded' onClick={() => listNFT(nft)}>List</button>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
+    return <h1 className="py-10 px-20 text-3x1">No NFTs owned yet</h1>;
+  return (
+    <div className="flex justify-center">
+      <div className="p-4">
+        <div className="grid grid-cols-1 sm:grid-cols02 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-x1 overflow-hidden">
+              <img src={nft.image} className="rounded" />
+              <div className="p-4 bg-black">
+                <p className="text-2x1 font-bold text-white">
+                  Price -{nft.price} ETHER
+                </p>
+                <button
+                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                  onClick={() => listNFT(nft)}
+                >
+                  List
+                </button>
+              </div>
             </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
