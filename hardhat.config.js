e7370df6f503c3677ad13c6a03d48156e8b54213
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-19 23:08:44 -0400

    config harmony testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,10 +10,10 @@ module.exports = {
       chainId: 1337,
     },
     //  unused configuration
-    //  testnet: {
-    //    url: "https://api.s0.b.hmny.io",
-    //    accounts: [hmny_priv]
-    //  }
+    testnet: {
+      url: "https://api.s0.b.hmny.io",
+      accounts: [hmny_priv],
+    },
   },
   solidity: {
     version: "0.8.4",
