fe96d333950feb7576a699a6c2974c1d609c774c
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-28 02:39:11 -0400

    show user asset-page

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -1,17 +1,10 @@
-/**
- * Page for viewing the NFTs purchased by the User
- * NFT is retrieved using the fetchMyNFTs function in the solidity contract
- *
- * Main function: retrieve NFT owned by the User and render them on the "my-nfts page"
- */
-
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Web3Modal from "web3modal";
 import { useRouter } from "next/router";
 
-import { marketplaceAddress } from "../config.js";
+import { marketplaceAddress } from "../config";
 
 import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
 
@@ -22,10 +15,9 @@ export default function MyAssets() {
   useEffect(() => {
     loadNFTs();
   }, []);
-
   async function loadNFTs() {
     const web3Modal = new Web3Modal({
-      network: "testnet",
+      network: "mainnet",
       cacheProvider: true,
     });
     const connection = await web3Modal.connect();
@@ -43,7 +35,7 @@ export default function MyAssets() {
       data.map(async (i) => {
         const tokenURI = await marketplaceContract.tokenURI(i.tokenId);
         const meta = await axios.get(tokenURI);
-        let price = ethers.utils.formatUnits(i.price.toString(), "ether/one");
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
         let item = {
           price,
           tokenId: i.tokenId.toNumber(),
@@ -59,20 +51,23 @@ export default function MyAssets() {
     setLoadingState("loaded");
   }
   function listNFT(nft) {
-    router.push("/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}");
+    console.log("nft:", nft);
+    router.push(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`);
   }
   if (loadingState === "loaded" && !nfts.length)
-    return <h1 className="py-10 px-20 text-3x1">No NFTs owned yet</h1>;
+    return (
+      <h1 className="text-white py-10 px-20 text-3xl">No NFTs owned yet</h1>
+    );
   return (
     <div className="flex justify-center">
       <div className="p-4">
-        <div className="grid grid-cols-1 sm:grid-cols02 lg:grid-cols-4 gap-4 pt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {nfts.map((nft, i) => (
-            <div key={i} className="border shadow rounded-x1 overflow-hidden">
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
               <img src={nft.image} className="rounded" />
               <div className="p-4 bg-black">
-                <p className="text-2x1 font-bold text-white">
-                  Price -{nft.price} ETHER
+                <p className="text-2xl font-bold text-white">
+                  Price - {nft.price} Eth
                 </p>
                 <button
                   className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
