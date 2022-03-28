9bec42d66ff623fd04946893e63cae4177fc2362
liquidoxygen96 <liquidoxygen96@protonmail.com>
2022-03-28 02:44:08 -0400

    deployer update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
-const fs = require("fs");
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const hre = require("hardhat"); //imports hardhat
+const fs = require("fs"); // reads file
 
-  // We get the contract to deploy
+async function main() {
+  //const [deployer] = await.getSigners();
+  //console.log("contract owner:", deployer.address)
+
   const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
   const nftMarketplace = await NFTMarketplace.deploy();
-
   await nftMarketplace.deployed();
-
   console.log("nftMarketplace deployed to:", nftMarketplace.address);
 
-  //creates a file named "config.js" which will contain the NFTMarkeplace.sol smart contract address
   fs.writeFileSync(
     "./config.js",
-    'export const marketplaceAddress = "${nftMarketplace.address}"'
+    `
+  export const marketplaceAddress = "${nftMarketplace.address}"
+  `
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
